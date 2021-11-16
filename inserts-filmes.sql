65cea11bc78c945b22b5d9e55d5d7f033d3f81c2
cauamenezes <cauamenezes.07@gmail.com>
2021-11-16 10:19:34 -0300

    comandos select feitos

--- inserts-filmes.sql
+++ inserts-filmes.sql
@@ -19,22 +19,27 @@ insert into tblFilme (nome,
                     sinopse,
                     dataLancamento,
                     dataRelancamento,
-                    tituloOriginal
+                    tituloOriginal,
+                    qtde
                     ) values
-                    (	'VINGADORES: ULTIMATO',
-						'03:01:00',
-                        'Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. Com Tony Stark (Robert Downey Jr.) vagando perdido no espaço sem água nem comida, o Capitão América/Steve Rogers (Chris Evans) e a Viúva Negra/Natasha Romanov (Scarlett Johansson) precisam liderar a resistência contra o titã louco.',
-                        '2019-04-25',
-						'2019-07-11',
-                        'Avengers: Endgame'
+                    (	'UM SONHO DE LIBERDADE',
+						'02:20:00',
+                        'Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela. Ele é mandado para uma prisão que é o pesadelo de qualquer detento, a Penitenciária Estadual de Shawshank, no Maine. Lá ele irá cumprir a pena perpétua. Andy logo será apresentado a Warden Norton (Bob Gunton), o corrupto e cruel agente penitenciário, que usa a Bíblia como arma de controle e ao Capitão Byron Hadley (Clancy Brown) que trata os internos como animais. Andy faz amizade com Ellis Boyd Redding (Morgan Freeman), um prisioneiro que cumpre pena há 20 anos e controla o mercado negro da instituição.',
+                        '1995-01-25',
+                        null,
+                        'The Shawshank Redemption',
+                        2
 					),
-					(	'O SENHOR DOS ANÉIS - O RETORNO DO REI',
-						'03:21:00',
-						'Sauron planeja um grande ataque a Minas Tirith, capital de Gondor, o que faz com que Gandalf (Ian McKellen) e Pippin (Billy Boyd) partam para o local na intenção de ajudar a resistência. Um exército é reunido por Theoden (Bernard Hill) em Rohan, em mais uma tentativa de deter as forças de Sauron. Enquanto isso Frodo (Elijah Wood), Sam (Sean Astin) e Gollum (Andy Serkins) seguem sua viagem rumo à Montanha da Perdição, para destruir o Um Anel.',
-						'2003-12-25',
-						null,
-						'The Lord of the Rings: The Return of the King' 
+                    (	'FORREST GUMP - O CONTADOR DE HISTÓRIAS',
+						'02:20:20',
+                        'Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções. Por obra do acaso, ele consegue participar de momentos cruciais, como a Guerra do Vietnã e Watergate, mas continua pensando no seu amor de infância, Jenny Curran.',
+                        '1994-12-07',
+                        null,
+                        'Forrest Gump',
+                        4
 					);
+                    
+                    
 
 #Tabela Filme Genero                    
 insert into tblFilmeGenero (idFilme, idGenero)
--- select.sql
+++ select.sql
@@ -26,6 +26,16 @@ select filme.nome as nomeFilme, duracao, sinopse
 select nome, sinopse from tblFilme where idFilme = 1;
 
 /*
+	
+    Operadores de comparação
+		=
+        <
+        >
+        <=
+        >=
+        like
+        <> -> diferente
+    
 	like busca os dados pela igualdade, como se fosse o (=)
     
     like '%var' - busca pelo final do atributo
@@ -33,3 +43,112 @@ select nome, sinopse from tblFilme where idFilme = 1;
     like '%var%' - busca por qualquer parte do atributo (início, meio ou fim)
 */
 select nome, sinopse from tblFilme where nome like '%vingadores%';
+
+/*
+	Operadores lógicos
+		and
+		or
+        not
+*/
+
+alter table tblFilme
+	add column qtde int;
+    
+update tblFilme set qtde = 5
+	where idFilme = 1;
+    
+update tblFilme set qtde = 7
+	where idFilme = 2;
+    
+#Exemplos usando operadores de comparação e operadores lógicos
+select * from tblFilme
+	where qtde <= 5;
+    
+select nome, qtde from tblFilme
+	where qtde <> 5;
+    
+select nome, qtde from tblFilme
+	where not (qtde = 5);
+    
+select nome, qtde from tblFilme
+	where qtde >= 3 and qtde <= 6;
+    
+select nome, qtde from tblFilme
+	where not (qtde >= 3 and qtde <= 6);
+    
+#between - permite localizar por um range de valores
+#Exemplo para não ficar escrevendo o atributo duas vezes
+select nome, qtde from tblFilme
+	where qtde between 3 and 6;
+    
+select nome, qtde from tblFilme
+	where qtde not between 3 and 6;
+    
+select nome, qtde
+	from tblFilme
+	where qtde = 2 or
+    qtde = 4 or
+    qtde = 6 or
+    qtde = 7;
+    
+#in() - permite filtrar por uma sequência de valores, não precisando escrever o atributo várias vezes
+#OBS: Só funciona com o operador lógico OR
+select nome, qtde
+	from tblFilme
+    where qtde in(2, 4, 6, 7);
+    
+select nome, qtde
+	from tblFilme
+    where qtde not in(2, 4, 6, 7);    
+    
+#Ordenação de dados
+	#order by
+		#ASC - ordenação crescente
+        #DESC - ordenação decrescente
+        
+select * from tblFilme order by nome asc;
+select * from tblFilme order by nome desc;
+
+select nome, qtde
+	from tblFilme
+    where nome like '%de%' and
+		qtde in (2, 4, 6)
+		order by nome asc;
+        
+select nome, qtde
+	from tblFilme
+    where nome like '%de%' and
+		qtde in (2, 4, 6)
+		order by nome desc;
+        
+#limit - permite limitar a qtde de resulado na tela
+select * from tblFilme limit 2;
+
+#Encontrando o primeiro registro da tabela
+select * from tblFilme 
+	order by idFilme asc limit 1;
+
+#Encontrando o último registro da tabela
+select * from tblFilme 
+	order by idFilme desc limit 1;
+
+#MAX - retorna o maior valor de um atributo
+#MIN - retorna o menor valor de um atributo
+
+select min(qtde) as qtdeFilmes from tblFilme;
+select max(qtde) as qtdeFilmes from tblFilme;
+
+#count - retorna a qtde de itens do filtro (obs: ele ignora os valores null)
+select count(nome) as qtdeFilmes from tblFilme
+	where nome like '%de%';
+    
+select count(dataRelancamento) as qtdeDataRelancamento,
+		count(nome) as qtdeNomeFilme
+	from tblFilme;
+
+#sum - retorna a soma de todas as colunas que foram especficadas
+select sum(qtde) as soma from tblFilme;
+
+#avg - retorna a média de uma coluna
+#round - define a qtde de casas decimais após a vírgula
+select round(avg(qtde), 1) as media from tblFilme;
